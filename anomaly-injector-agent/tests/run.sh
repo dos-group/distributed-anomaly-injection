c8f8d6550911b9819a922e1f7e2c01f038578c73
Alexander Acker <alexander.acker@tu-berlin.de>
2019-04-02 16:34:06 +0200

    Updated test to use most up to date container.

--- anomaly-injector-agent/tests/run.sh
+++ anomaly-injector-agent/tests/run.sh
@@ -3,5 +3,6 @@
 REPO=teambitflow/anomaly-injector-agent
 
 #docker build -t "$REPO" ../
+docker pull $REPO
 docker start $(docker create --net="host" --pid="host" --privileged "$REPO" -host=wally166 -api-port 7999)
 
