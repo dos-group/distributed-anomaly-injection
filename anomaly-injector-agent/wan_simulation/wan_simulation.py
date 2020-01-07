9c28e6af988aa68869694f75316393a4664350c9
Soeren Becker <soeren.becker@tu-berlin.de>
2020-01-07 16:56:13 +0100

    Remove not used methods

--- anomaly-injector-agent/wan_simulation/wan_simulation.py
+++ anomaly-injector-agent/wan_simulation/wan_simulation.py
@@ -20,19 +20,5 @@ class WanSimulation(object):
     def is_running(self):
         raise NotImplementedError("Abstract method")
 
-    def auto_revert(self, auto_revert_time):
-        p = Thread(target=self.__auto_revert, args=(auto_revert_time,))
-        p.start()
-
-    def __auto_revert(self, auto_revert_time):
-        if int(auto_revert_time) > 0:
-            logging.info("Started auto-reverter. Reverting after " + auto_revert_time + " seconds")
-            time.sleep(int(auto_revert_time))
-            if self.running:
-                logging.info("Auto-reverting...")
-                self.revert()
-                if not self.status():
-                    logging.info("Auto-revert successful")
-
     def __str__(self):
         return self.interface
